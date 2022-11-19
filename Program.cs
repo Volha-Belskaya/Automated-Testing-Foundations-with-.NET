1c564af74de67016b98702a45058a1065537edef
Volha Belskaya <volha.belskaya.lt@gmail.com>
2022-11-19 21:40:10 +0200

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+// since int stores 32 bits, the longest number will be at most 32 characters
+int[] remainder_of_the_division = new int[32];
+
+    int i = 31;
+    while (number > 0)
+    {
+        remainder_of_the_division[i] = number % b;
+        number = number / b;
+        i = i - 1;
+    }
+    Console.WriteLine();
+
+for (int j = 0; j < remainder_of_the_division.Length; j++)
+{
+    Console.Write(" " + remainder_of_the_division[j]);
+}
